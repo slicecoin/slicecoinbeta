f6dfd5e1a43e3dbba55425de64ac9ecf828ac208
slicecoin <slicecoin@outlook.com>
2014-02-23 14:36:09 +0100

    Second commit

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #smallchangeTEST3\r");
-            Send(hSocket, "WHO #smallchangeTEST3\r");
+            Send(hSocket, "JOIN #betacoinTEST3\r");
+            Send(hSocket, "WHO #betacoinTEST3\r");
         } else {
-            // randomly join #smallchange00-#smallchange99
+            // randomly join #betacoin00-#betacoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #smallchange%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #smallchange%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #betacoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #betacoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
